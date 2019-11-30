2f01c4fdbec3f9f5f5689e61516a4419d70ade25
kimyongjin <yongjin5184@gmail.com>
2019-11-30 22:01:55 +0900

    jpa example
    1. instagram modeling

---
+++ src/main/java/com/example/jpa_example/model/entity/instagram/post/PostId.java
@@ -0,0 +1,18 @@
+package com.example.jpa_example.model.entity.instagram.post;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.GeneratedValue;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class PostId implements Serializable {
+    @Column(name = "post_id")
+    @GeneratedValue
+    @EmbeddedId
+    private Long id;
+}
